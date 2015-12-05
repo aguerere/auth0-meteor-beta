64317d70a4c3dcb2f777108ffdfd4b4189882f02
Alberto Güerere <alberto.guerere@techaid.co>
2015-12-04 20:31:56 -0430

    Fixed an issue to run the package on Windows
    Replaced the dotenv Meteor package by the npm one, which is current.
    Added a function to determine the host app absolute path, so the package
    can locate the .env file and load the values correctly, on any supported
    server platform.

--- auth0-meteor-sample/packages/lock/auth0-lock.server.js
+++ auth0-meteor-sample/packages/lock/auth0-lock.server.js
@@ -1,14 +1,23 @@
-// Write your package code here!
 if (Meteor.isServer) {
-    Meteor.startup(function () {
-    });
+    // Calculate the absolute path for the app
+    var _getAppPath = function(){
+        // On Windows the tokenizer must include a backslash
+        if (process.platform === 'win32' || process.platform === 'win64') {
+          return __meteor_bootstrap__.serverDir.split('\\.meteor')[0] + '\\';
+        }
+        return __meteor_bootstrap__.serverDir.split('/.meteor')[0] + '/';
+    };
 
-    // Register the Auth0 login handler for Meteor
+    // Load the .env file
+    Npm.require('dotenv').load({path: _getAppPath() + '.env'});
+
+    // Register the Auth0 login handler for Meteor.
     Accounts.registerLoginHandler(function (options) {
         if (!options.auth0)
             return undefined; // Do not handle
 
         if (!_.contains(Accounts.oauth.serviceNames(), 'auth0')) {
+            // If the auth0 service was removed from serviceConfiguration
             return {
                 type : "auth0",
                 error: new Meteor.Error(
@@ -17,18 +26,23 @@ if (Meteor.isServer) {
             }
         }
 
-        // Do nothing if the profile is not received
+        // Do nothing if the profile is not received.
         if (!options.auth0.profile || !options.auth0.profile.user_id)
             return null;
 
-        // Accounts.updateOrCreateUserFromExternalService expects the unique user id
-        // to be stored in the 'id' property of serviceData
+        // Accounts.updateOrCreateUserFromExternalService 
+        // expects the unique user id to be stored in the 'id'
+        // property of serviceData.
         options.auth0.profile.id = options.auth0.profile.user_id;
 
-
+        // Run the Accounts method to store the profile and 
+        // optional data (token) in Meteor users collection.
         return Accounts.updateOrCreateUserFromExternalService("auth0", options.auth0.profile, options.auth0.token);
     });
 
+    // Send the user details to the client. Complete profile when logged in user,
+    // only id and name to all other users. This data is stored in the local users collection.
+    // It is created or updated locally when logging in.
     Accounts.addAutopublishFields({
         forLoggedInUser: ['services.auth0'],
         forOtherUsers: [
@@ -36,7 +50,10 @@ if (Meteor.isServer) {
         ]
     });
 
+    // Publish server methods
     Meteor.methods({
+        // Sends the Auth0 account attributes to the client upon request.
+        // Gets called from the Meteor.startup function on the client.
         'getAuth0Attributes': function () {
             return {
                 AUTH0_CLIENTID: process.env.AUTH0_CLIENTID,
